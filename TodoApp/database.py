9fb7ceb844c1f882e28c4362f5c44349b3ba5664
kok4depelover <jimenezsaul2004@gmail.com>
2025-06-12 23:36:50 -0600

    Prod database change

--- TodoApp/database.py
+++ TodoApp/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-SQLALCHEMY_DATABASE_URL = 'sqlite:///./todosapp.db'
+SQLALCHEMY_DATABASE_URL = 'postgresql://deply_database_user:lYq5e3NQNdbXfYtm96YuZ86ycxyiQPyN@dpg-d15rfe6mcj7s73dnehb0-a/deply_database'
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={'check_same_thread': False})
 
